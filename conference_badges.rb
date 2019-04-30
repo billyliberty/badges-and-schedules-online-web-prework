18efeb3e5644d529f4adbdf2351142f6526d4644
Joshua Crum <joshua.crum@gmail.com>
2019-04-30 16:26:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+def batch_badge_creator(name)
+  name = [Edsger, Ada, Charles, Alan, Grace, Linus, and Matz]
+  puts "Hello, my name is ${name} ."
+end
+
+def assign_rooms
+end
+
+def printer
+end
