6fbddcf7c9256aecad31a4a0c2c9393862485964
Joshua Crum <joshua.crum@gmail.com>
2019-04-30 16:44:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,18 @@
 
 
 def badge_maker(name)
-  puts "Hello, my name is #{name}."
+  return "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(attendees)
+  attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  attendees.each do |name|
+    puts badge_maker(name)
+  end
+end
+
+def assign_rooms
+  each_with_index
+end
+
+def printer
