eff2f72e9a3d9a8b9bb5f18a6d61841880a9595e
Joshua Crum <joshua.crum@gmail.com>
2019-04-30 16:53:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,9 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
+  badge_array = []
   attendees.each do |name|
-    puts badge_maker(name)
+    badge_array.push("Hello, my name is #{name}.")
   end
+  return badge_array
 end
